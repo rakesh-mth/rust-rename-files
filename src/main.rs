33e4039941df02ca3f6544e8387854b59ad2e302
Rakesh Kumar <rakesh.mth@gmail.com>
2019-06-23 00:54:55 -0400

    add rename cha files to txt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+extern crate glob;
+
+use glob::glob;
+use clap::{Arg, App, AppSettings};
+
+use std::fs;
+
+fn main() 
+{
+  let path_arg_name = "path";
+  let args = App::new("cha-to-txt")
+    .about("Rename .cha to .txt")
+    .setting(AppSettings::ArgRequiredElseHelp)
+    .arg(Arg::with_name(path_arg_name)
+    .help("path to the top directory with .cha files"))
+    .get_matches();
+
+  let path = args.value_of(path_arg_name)
+    .expect("You didn't supply a path");
+  let search = String::from(path) + "/**/*.cha";
+  let paths = glob(&search)
+    .expect("Could not find paths in glob")
+    .map(|p| p.expect("Bad individual path in glob"));
+
+  for path in paths {
+    match fs::rename(&path, &path.with_extension("txt")) {
+      Ok(_) => (),
+      Err(reason) => panic!("{}", reason),
+    };
+  }
 }
