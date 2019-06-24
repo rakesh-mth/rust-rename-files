6e8e177094567b748e79ebabbd6efc36d031c92c
Rakesh Kumar <rakesh.mth@gmail.com>
2019-06-23 21:54:38 -0400

    rename any extention

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,30 @@ use std::fs;
 fn main() 
 {
   let path_arg_name = "path";
-  let args = App::new("cha-to-txt")
-    .about("Rename .cha to .txt")
+  let path_arg_src_ext = "src_fmt";
+  let path_arg_dst_ext = "dst_fmt";
+  let args = App::new("src-fmt-to-dst-fmt")
+    .about("Rename .src_fmt to .dst_fmt")
     .setting(AppSettings::ArgRequiredElseHelp)
-    .arg(Arg::with_name(path_arg_name)
-    .help("path to the top directory with .cha files"))
+    .arg(Arg::with_name(path_arg_name).help("path to the top directory with .cha files"))
+    .arg(Arg::with_name(path_arg_src_ext).help("source file extention"))
+    .arg(Arg::with_name(path_arg_dst_ext).help("destination file extention"))
     .get_matches();
 
   let path = args.value_of(path_arg_name)
     .expect("You didn't supply a path");
-  let search = String::from(path) + "/**/*.cha";
+  let src_ext = args.value_of(path_arg_src_ext)
+    .expect("You didn't supply source extention");
+  let dst_ext = args.value_of(path_arg_dst_ext)
+    .expect("You didn't supply destination extention");
+
+  let search = String::from(path) + "/**/*." + src_ext;
   let paths = glob(&search)
     .expect("Could not find paths in glob")
     .map(|p| p.expect("Bad individual path in glob"));
 
   for path in paths {
-    match fs::rename(&path, &path.with_extension("txt")) {
+    match fs::rename(&path, &path.with_extension(dst_ext)) {
       Ok(_) => (),
       Err(reason) => panic!("{}", reason),
     };
